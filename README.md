1e1cc14ba9b7e8c6fc908eb1ea18269d949e8214
Rajat Sharma <er.rajat15@gmail.com>
2020-08-23 21:53:31 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# React pinterest app
---
+++ src/components/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>React Starter</h1>
+
+      <h2>Get Started</h2>
+    </div>
+  );
+};
+
+export default HomePage;
